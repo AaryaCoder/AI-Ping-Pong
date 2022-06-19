0f8ecd865b2996d380d13a541c7e43da981ea1e9
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2022-06-19 09:10:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,19 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video=createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  poseNet=ml5.poseNet(video,modelLoaded);
+  poseNet.on('poses',gotPoses);
+}
+function modelLoaded(){
+  console.log("poseNet is initialized!");
+}
+function gotPoses(results){
+  
 }
 
 
@@ -162,3 +175,4 @@ function paddleInCanvas(){
     mouseY =0;
   }  
 }
+
